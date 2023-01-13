e32fe997db64be64751348594b43836ac524cc6e
Enock Nkumbuyedeni Nsanzimfura <nkumbuyenock@gmail.com>
2023-01-13 18:21:21 +0200

    Revert "Final Project Submission (Movies Hub)"

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './style.css';
-import { retrieveMovie, navigation } from './modules/display-cards.js';
-import { RenderpopUp } from './modules/render-popup.js';
-
-retrieveMovie();
-RenderpopUp();
-navigation();
